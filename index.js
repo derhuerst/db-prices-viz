5496fa236db37afc73af163d30d7229c6dc28248
Jannis R <mail@jannisr.de>
2017-05-13 00:24:54 +0200

    HTTP server :sparkles:

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const http = require('http')
-const httpServer = require('./http')
+const url = require('url')
+const through = require('through2')
+const pick = require('lodash.pick')
+const ndjson = require('ndjson')
+
+const readJourneys = require('./lib/read-journeys')
 
 const showError = (err) => {
 	console.error(err)
@@ -10,8 +15,33 @@ const showError = (err) => {
 
 const port = process.env.PORT || 3000
 
+const err400 = (res, msg) => {
+	console.error(msg)
+	res.writeHead(400, {'content-type': 'application/json'})
+	res.end(JSON.stringify({error: true, msg}))
+}
+
 const server = http.createServer((req, res) => {
-	res.json({})
+	const {query} = url.parse(req.url, true)
+	const {origin, destination, departure, arrival} = query
+
+	if (!origin) return err400(res, 'Missing origin parameter.')
+	if (!destination) return err400(res, 'Missing destination parameter.')
+
+	if (!departure) return err400(res, 'Missing departure parameter.')
+	if (isNaN(+new Date(departure))) return err400(res, 'departure must be a date string.')
+	if (!arrival) return err400(res, 'Missing arrival parameter.')
+	if (isNaN(+new Date(arrival))) return err400(res, 'arrival must be a date string.')
+
+	if (!query.lines) return err400(res, 'Missing lines parameter.')
+	const lines = query.lines.split(',')
+
+	readJourneys({origin, destination, departure, arrival, lines})
+	.pipe(through.obj((journey, _, cb) => {
+		cb(null, pick(journey, ['trips', 'offer', 'requestDate']))
+	}))
+	.pipe(ndjson.stringify())
+	.pipe(res)
 })
 
 server.listen(port, (err) => {
